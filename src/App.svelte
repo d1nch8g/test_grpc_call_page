35f1041281194a0d1daa04f7c925b8c054a09fa7
DANCHE <dangdancheg@gmail.com>
2022-07-17 22:49:33 +0300

    built vscode stuff with svelte

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  const vscode = acquireVsCodeApi();
+
+  vscode.postMessage({
+    command: "alert",
+    text: "🐛 das me ",
+  });
+
+  window.addEventListener("message", (event) => {
+    console.log(event.data);
+  });
+  
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src="https://grpc.io/img/logos/grpc-icon-color.png" class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
